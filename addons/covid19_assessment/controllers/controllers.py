3ec41a86d2dee745565de39deacd8f54bee44753
AnzeSavli <anze.savli99@gmail.com>
2022-04-20 20:12:18 +0200

    first commit

--- addons/covid19_assessment/controllers/controllers.py
+++ addons/covid19_assessment/controllers/controllers.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from requests import request
+from odoo import http
+
+class Covid19Assessment(http.Controller):
+    @http.route('/covid19_assessment/covid19_assessment/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
+
+    # @http.route('create/user', type="http", auth="public", website=True)
+    # def create_user(self, **kw):
+    #     return request.render("covid19_assessment.user_registered", {})
+
+    @http.route('/covid19_assessment/covid19_assessment/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('covid19_assessment.listing', {
+            'root': '/covid19_assessment/covid19_assessment',
+            'objects': http.request.env['covid19_assessment.covid19_assessment'].search([]),
+        })
+
+    @http.route('/covid19_assessment/covid19_assessment/objects/<model("covid19_assessment.covid19_assessment"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('covid19_assessment.object', {
+            'object': obj
+        })
 
 
-# class Covid19Assessment(http.Controller):
-#     @http.route('/covid19_assessment/covid19_assessment/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
 
-#     @http.route('/covid19_assessment/covid19_assessment/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('covid19_assessment.listing', {
-#             'root': '/covid19_assessment/covid19_assessment',
-#             'objects': http.request.env['covid19_assessment.covid19_assessment'].search([]),
-#         })
-
-#     @http.route('/covid19_assessment/covid19_assessment/objects/<model("covid19_assessment.covid19_assessment"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('covid19_assessment.object', {
-#             'object': obj
-#         })
--- addons/covid19_assessment/models/models.py
+++ addons/covid19_assessment/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class covid19_assessment(models.Model):
-#     _name = 'covid19_assessment.covid19_assessment'
-#     _description = 'covid19_assessment.covid19_assessment'
+class covid19_assessment(models.Model):
+    _name = 'covid19_assessment.covid19_assessment'
+    _description = 'covid19_assessment.covid19_assessment'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    firstName = fields.Char(string="What is your first name?")
+    lastName = fields.Char(string="What is your last name?")
+    age = fields.Integer(string="What is your age?")
+    email = fields.Char(string="Please enter your email address")
+    fever = fields.Boolean(string="Do you have a fever?", default=False)
+    temperature = fields.Float(string="What is your last measured temperature?")
+    cough = fields.Boolean(string="Do you cough?")
+    headache = fields.Boolean(string="Do you have sever headache?")
+    contact = fields.Boolean(string="Were in contact with someone who has COVID-19?")
